d03565749af6f1054d76f4e216ed9649774f2056
EddieGilXD <djuntaied@gmail.com>
2024-07-19 21:10:03 -0600

    Initial commit

---
+++ Trabajos/To-do list completado/app.js
@@ -0,0 +1,93 @@
+class Node {
+    constructor(task) {
+        this.task = task;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    
+    addTask(task) {
+        const newNode = new Node(task);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    
+    removeTask(index) {
+        if (index === 0 && this.head) {
+            this.head = this.head.next;
+            return;
+        }
+        let current = this.head;
+        let previous = null;
+        let count = 0;
+
+        while (current && count < index) {
+            previous = current;
+            current = current.next;
+            count++;
+        }
+
+        if (current) {
+            previous.next = current.next;
+        }
+    }
+    
+    getTasks() {
+        const tasks = [];
+        let current = this.head;
+
+        while (current) {
+            tasks.push(current.task);
+            current = current.next;
+        }
+
+        return tasks;
+    }
+}
+
+const taskList = new LinkedList();
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const task = taskInput.value;
+
+    if (task) {
+        taskList.addTask(task);
+        taskInput.value = '';
+        displayTasks();
+    }
+}
+
+function removeTask(index) {
+    taskList.removeTask(index);
+    displayTasks();
+}
+
+function displayTasks() {
+    const tasks = taskList.getTasks();
+    const taskListElement = document.getElementById('taskList');
+    taskListElement.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = task;
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Eliminar';
+        removeButton.onclick = () => removeTask(index);
+        listItem.appendChild(removeButton);
+        taskListElement.appendChild(listItem);
+    });
+}
+
+displayTasks();
